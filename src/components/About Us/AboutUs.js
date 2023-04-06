2a4c1dec659e319e319d232fabb1e07f0401f9d3
joeeeee7 <johanansamuel76@gmail.com>
2023-04-06 15:38:17 +0800

    added 4 cards

--- src/components/About Us/AboutUs.js
+++ src/components/About Us/AboutUs.js
@@ -1,5 +1,36 @@
-function AboutUs() {
-  return <h2>The Heroic Archive</h2>;
-}
+import { display } from "@mui/system";
+import ProfileCard from "./Card";
 
+function AboutUs() {
+  return (
+    <div className="wrapper-container">
+      <div className="flex-container">
+        <div className="first-box">
+          <ProfileCard />
+        </div>
+        <div className="second-box">
+          <ProfileCard />
+        </div>
+        <div className="third-box">
+          <ProfileCard />
+        </div>
+        <div className="fourth-box">
+          <ProfileCard />
+        </div>
+      </div>
+      <div className="text-about">
+        <p>Lorem ergregeegege</p>
+        <p>Lorem gegegggeg</p>
+        <p>Lorem gegeggeeg</p>
+        <p>Lorem gegegeggeg</p>
+      </div>
+      <div className="btn-divider">
+        <button className="btn btn-primary">View More</button>
+        <button className="btn btn-primary">View More</button>
+        <button className="btn btn-primary">View More</button>
+        <button className="btn btn-primary">View More</button>
+      </div>
+    </div>
+  );
+}
 export default AboutUs;
