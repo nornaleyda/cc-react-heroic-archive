ae5eb450e88170ba28a2dc8de661d825b5c44e38
nzmksk <mhafiz9676@gmail.com>
2023-04-10 23:56:25 +0800

    Add filter checkbox

--- src/components/Characters/FilterByRace.js
+++ src/components/Characters/FilterByRace.js
@@ -1,105 +1,80 @@
 import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
 import { useState } from "react";
+import { useToggle } from "../../hooks/useToggle";
 
 export default function FilterByRace() {
-  const [allChecked, setAllChecked] = useState([
-    true,
-    true,
-    true,
-    true,
-    true,
-    true,
-    true,
-  ]);
+  const [raceFilters, setRaceFilters] = useState([]);
+  const [filterHuman, toggleFilterHuman] = useToggle(false);
+  const [filterMutant, toggleFilterMutant] = useToggle(false);
+  const [filterRadiation, toggleFilterRadiation] = useToggle(false);
+  const [filterCyborg, toggleFilterCyborg] = useToggle(false);
+  const [filterEternal, toggleFilterEternal] = useToggle(false);
+  const [filterUnknown, toggleFilterUnknown] = useToggle(false);
+  const [filterOthers, toggleFilterOthers] = useToggle(false);
 
-  const handleCheck = (event, index) => {
-    const newChecked = [...allChecked];
-    newChecked[index] = event.target.checked;
-    setAllChecked(newChecked);
-  };
+  const checkboxList = [
+    {
+      race: "Human",
+      filter: filterHuman,
+      toggle: toggleFilterHuman,
+    },
+    {
+      race: "Mutant",
+      filter: filterMutant,
+      toggle: toggleFilterMutant,
+    },
+    {
+      race: "Radiation",
+      filter: filterRadiation,
+      toggle: toggleFilterRadiation,
+    },
+    {
+      race: "Cyborg",
+      filter: filterCyborg,
+      toggle: toggleFilterCyborg,
+    },
+    {
+      race: "Eternal",
+      filter: filterEternal,
+      toggle: toggleFilterEternal,
+    },
+    {
+      race: "Unknown",
+      filter: filterUnknown,
+      toggle: toggleFilterUnknown,
+    },
+    {
+      race: "Others",
+      filter: filterOthers,
+      toggle: toggleFilterOthers,
+    },
+  ];
 
-  return (
-    <FormGroup>
-      <FormControlLabel
-        label="All"
-        control={
-          <Checkbox
-            checked={
-              allChecked[0] &&
-              allChecked[1] &&
-              allChecked[2] &&
-              allChecked[3] &&
-              allChecked[4] &&
-              allChecked[5] &&
-              allChecked[6]
+  const renderCheckbox = () => {
+    return checkboxList.map((checkbox, index) => {
+      return (
+        <FormControlLabel
+          label={checkbox.race}
+          name={checkbox.race}
+          value={checkbox.race}
+          key={index}
+          control={<Checkbox checked={checkbox.filter} />}
+          onClick={(event) => {
+            checkbox.toggle();
+            if (event.target.checked) {
+              setRaceFilters([...raceFilters, event.target.value]);
+            } else {
+              setRaceFilters(
+                raceFilters.filter(
+                  (filterType) => filterType !== event.target.value
+                )
+              );
             }
-            // indeterminate={}
-          />
-        }
-      />
-      <FormControlLabel
-        label="Human"
-        control={
-          <Checkbox
-            checked={allChecked[0]}
-            onChange={(event) => handleCheck(event, 0)}
-          />
-        }
-      />
-      <FormControlLabel
-        label="Mutant"
-        control={
-          <Checkbox
-            checked={allChecked[1]}
-            onChange={(event) => handleCheck(event, 1)}
-          />
-        }
-      />
-      <FormControlLabel
-        label="Radiation"
-        control={
-          <Checkbox
-            checked={allChecked[2]}
-            onChange={(event) => handleCheck(event, 2)}
-          />
-        }
-      />
-      <FormControlLabel
-        label="Cyborg"
-        control={
-          <Checkbox
-            checked={allChecked[3]}
-            onChange={(event) => handleCheck(event, 3)}
-          />
-        }
-      />
-      <FormControlLabel
-        label="Eternal"
-        control={
-          <Checkbox
-            checked={allChecked[4]}
-            onChange={(event) => handleCheck(event, 4)}
-          />
-        }
-      />
-      <FormControlLabel
-        label="Unknown"
-        control={
-          <Checkbox
-            checked={allChecked[5]}
-            onChange={(event) => handleCheck(event, 5)}
-          />
-        }
-      />
-      <FormControlLabel
-        label="Others"
-        control={
-          <Checkbox
-            checked={allChecked[6]}
-            onChange={(event) => handleCheck(event, 6)}
-          />
-        }
-      />
-    </FormGroup>
-  );
+          }}
+        />
+      );
+    });
+  };
+  
+  return <FormGroup>{renderCheckbox()}</FormGroup>;
 }
