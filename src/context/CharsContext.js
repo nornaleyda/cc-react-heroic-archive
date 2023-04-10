dbdbab424c88b9195f55d0da807621ba0bc36c46
nzmksk <mhafiz9676@gmail.com>
2023-04-11 00:29:54 +0800

    Code refactoring

--- src/context/CharsContext.js
+++ src/context/CharsContext.js
@@ -1,5 +1,6 @@
 import { createContext, useContext, useRef, useState } from "react";
 import axios from "axios";
+import { useToggle } from "../hooks/useToggle";
 
 const CharsContext = createContext();
 
@@ -11,28 +12,28 @@ export function CharsContextProvider({ children }) {
   );
   const [searchInput, setSearchInput] = useState("");
   const [filteredResults, setFilteredResults] = useState(allCharacters);
+  
+  // SORTING HOOKS CONTEXT
   const anchorRef = useRef(null);
-  const [openPopper, setOpenPopper] = useState(false);
+  const [openPopper, togglePopper] = useToggle(false);
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [sortingMethod, setSortingMethod] = useState({
     sort: sortingOptions[selectedIndex],
     reverseOrder: false,
   });
-  const [COTD, setCOTD] = useState(null);
-  const randomIndex = Math.random() * 562;
 
-  // Retrieve all characters
+  // Retrieve all characters from API
   const retrieveCharacters = async () => {
     try {
       await axios
         .get("https://akabab.github.io/superhero-api/api/all.json")
         .then((response) => {
           const data = response.data;
-          if (allCharacters === []) {
-            setAllCharacters(data);
-            // Store the full API data in local storage
-            localStorage.setItem("heroic-archive", JSON.stringify(data));
-          }
+          setAllCharacters(data);
+
+          // Store the full API data in local storage
+          localStorage.setItem("heroic-archive", JSON.stringify(data));
+
           // Filter the data based on user's input
           searchInput !== ""
             ? setFilteredResults(
@@ -60,33 +61,27 @@ export function CharsContextProvider({ children }) {
     }
   };
 
+  // SORTING CONTEXT
   const handleSortButtonClick = () => {
     setSortingMethod({
       ...sortingMethod,
       reverseOrder: !sortingMethod.reverseOrder,
     });
   };
-
-  const handleTogglePopper = () => {
-    setOpenPopper((prevOpen) => !prevOpen);
-  };
-
   const handleMenuItemClick = (event, index) => {
     setSelectedIndex(index);
-    setOpenPopper(false);
+    togglePopper();
     setSortingMethod({
       sort: sortingOptions[index],
       reverseOrder: false,
     });
   };
-
   const handleClosePopper = (event) => {
     if (anchorRef.current && anchorRef.current.contains(event.target)) {
       return;
     }
-    setOpenPopper(false);
+    togglePopper();
   };
-
   const switchSorting = (sortType, sortOrder, data) => {
     switch (sortType) {
       case "INT":
@@ -140,14 +135,12 @@ export function CharsContextProvider({ children }) {
   };
 
   const contextValue = {
-    allCharacters,
     anchorRef,
     filteredResults,
     handleClosePopper,
     handleMenuItemClick,
     handleSearchInput,
     handleSortButtonClick,
-    handleTogglePopper,
     openPopper,
     retrieveCharacters,
     searchInput,
@@ -155,6 +148,7 @@ export function CharsContextProvider({ children }) {
     sortingMethod,
     sortingOptions,
     switchSorting,
+    togglePopper,
   };
 
   return (
