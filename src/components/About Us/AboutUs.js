f3caf70129edfbc32670615510b28397d65bc48b
nzmksk <mhafiz9676@gmail.com>
2023-04-10 09:58:13 +0800

    Add GitHub and LinkedIn link

--- src/components/About Us/AboutUs.js
+++ src/components/About Us/AboutUs.js
@@ -8,12 +8,20 @@ const devDetails = [
     biography:
       "Watch out, villains - Hafiz is on the scene! Despite coming from a health science background, this self-taught coding genius has one thing in common with the Spider-Man - making websites. Starting his coding journey back in June 2022, he ventured out into the jungle of data analytics with Python. It wasn't long before Hafiz started swinging into web development, quickly mastering HTML, CSS, and JavaScript. Now, he's expanding his arsenal with React.js, Express.js, and Django. Whether you're a hero or a villain, Hafiz is the coder you want on your side!",
     image: "",
+    url: {
+      github: "https://github.com/nzmksk",
+      linkedIn: "https://www.linkedin.com/in/mhz96/",
+    },
   },
   {
     id: 2,
     name: "Johanan Samuel",
     biography: "",
     image: "",
+    url: {
+      github: "",
+      linkedIn: "",
+    },
   },
   {
     id: 3,
@@ -21,6 +29,10 @@ const devDetails = [
     biography:
       "Norna's superpower is her proficiency in coding languages. Armed with a bachelor's degree in Science Computer: Multimedia Computer, she quickly mastered HTML, CSS, and Javascript. Now, Norna is focused on expanding her abilities with React.js, Typescript, and sass in order to become the ultimate front-end developer. With her unwavering determination and unstoppable drive to save the world, Norna is a true hero in the making.",
     image: "",
+    url: {
+      github: "",
+      linkedIn: "",
+    },
   },
   {
     id: 4,
@@ -28,6 +40,10 @@ const devDetails = [
     biography:
       "Hana, the villain with a knack for technology, has used her skills to cause chaos in the digital realm. She's a master of HTML, CSS, and JavaScript and has a deep understanding of frontend frameworks and libraries, such as React. Her cunning problem-solving skills and strategic mind make her a formidable opponent to any superhero. Hana stays up-to-date with the latest frontend development trends and techniques, giving her a significant advantage in her villainous pursuits.",
     image: "",
+    url: {
+      github: "",
+      linkedIn: "",
+    },
   },
 ];
 
