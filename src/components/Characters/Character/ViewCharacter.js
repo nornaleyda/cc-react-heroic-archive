48496348a328f3324f0fa47052683372aba062ce
FarhanaAlias <128573444+FarhanaAlias@users.noreply.github.com>
2023-04-11 10:20:20 +0800

    Hana (#28)
    * add new js component in COTD
    
    * Update DisplayResults.js
    
    ---------
    
    Co-authored-by: Mohd Hafiz Zabba <107503836+nzmksk@users.noreply.github.com>

--- src/components/Characters/Character/ViewCharacter.js
+++ src/components/Characters/Character/ViewCharacter.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Typography, LinearProgress } from "@mui/material";
+import { Typography, Paper, LinearProgress, colors } from "@mui/material";
 import { useParams } from "react-router-dom";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 export default function ViewCharacter() {
   const [character, setCharacter] = useState(null);
@@ -38,114 +41,221 @@ export default function ViewCharacter() {
     images,
   } = character;
 
+
   return (
-    <div>
-      <Typography variant="h4" style={{ marginBottom: "1rem" }}>
-        {name}
-      </Typography>
-      <img
-        src={images.md}
-        alt={name}
-        style={{
-          marginBottom: "1rem",
-          animation: "pulse 2s infinite",
+    <Box
+      sx={{
+        marginTop: "5rem",
+        marginBottom: "5rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Paper
+        sx={{
+          position: "relative",
+          p: 3,
+          margin: "auto",
+          maxWidth: "70%",
+          flexGrow: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+          animation: "fade-in 1s forwards",
+          "@keyframes fade-in": {
+            "0%": {
+              opacity: 0,
+            },
+            "100%": {
+              opacity: 1,
+            },
+          },
         }}
-      />
-      <Typography variant="h6">Appearance</Typography>
-      <Typography>{`Gender: ${appearance.gender}, Species: ${appearance.race}, Eyes: ${appearance["eye-color"]}, Hair: ${appearance["hair-color"]}`}</Typography>
-      <Typography>{`Height: ${appearance.height.join(
-        ", "
-      )}, Weight: ${appearance.weight.join(", ")}`}</Typography>
-      <br />
-      <Typography variant="h6">Biography</Typography>
-      <Typography>Full Name: {character.biography.fullName}</Typography>
-      <Typography>Alter Ego: {character.biography.alterEgos}</Typography>
-      <Typography>
-        Aliase(s): {character.biography.aliases.join(", ")}
-      </Typography>
-      <Typography>
-        Place Of Birth: {character.biography.placeOfBirth}
-      </Typography>
-      <Typography>
-        First Appearance(s): {character.biography.firstAppearance}
-      </Typography>
-
-      <Typography>{`Publisher: ${biography.publisher}`}</Typography>
-      <Typography>{`Alignment: ${biography.alignment}`}</Typography>
-      <br />
-      <Typography variant="h6">Powerstats</Typography>
-
-      <Typography sx={{ width: "50%" }}>
-        <label sx={{ width: "200px", textAlign: "right" }}>
-          Intelligence : {powerstats.intelligence}%
-        </label>
-        <LinearProgress
-          sx={{ height: 15, borderRadius: 5 }}
-          variant="determinate"
-          value={powerstats.intelligence}
-        />
-        <br />
-        <label sx={{ width: "200px", textAlign: "right" }}>
-          Strength : {powerstats.strength}%
-        </label>
-        <LinearProgress
-          sx={{ height: 15, borderRadius: 5 }}
-          variant="determinate"
-          value={powerstats.strength}
-        />
-        <br />
-        <label sx={{ width: "200px", textAlign: "right" }}>
-          Speed : {powerstats.speed}%
-        </label>
-        <LinearProgress
-          sx={{ height: 15, borderRadius: 5 }}
-          variant="determinate"
-          value={powerstats.speed}
-        />
-        <br />
-        <label sx={{ width: "200px", textAlign: "right" }}>
-          Durability : {powerstats.durability}%
-        </label>
-        <LinearProgress
-          sx={{ height: 15, borderRadius: 5 }}
-          variant="determinate"
-          value={powerstats.durability}
-        />
-        <br />
-        <label sx={{ width: "200px", textAlign: "right" }}>
-          Power : {powerstats.power}%
-        </label>
-        <LinearProgress
-          sx={{ height: 15, borderRadius: 5 }}
-          variant="determinate"
-          value={powerstats.power}
-        />
-        <br />
-        <label sx={{ width: "200px", textAlign: "right" }}>
-          Combat : {powerstats.combat}%
-        </label>
-        <LinearProgress
-          sx={{ height: 15, borderRadius: 5 }}
-          variant="determinate"
-          value={powerstats.combat}
-        />
-      </Typography>
-
-      <br />
-      <Typography variant="h6">Work</Typography>
-      <Typography>{`Occupation: ${work.occupation}`}</Typography>
-      <Typography>{`Base: ${work.base}`}</Typography>
-      <br />
-      <Typography variant="h6">Connections</Typography>
-      <Typography>{`Group Affiliation: ${connections["group-affiliation"]}`}</Typography>
-      <Typography>{`Family: ${connections.relatives}`}</Typography>
-      <Typography
-        variant="h5"
-        style={{ marginTop: "2rem", fontStyle: "italic" }}
       >
-        "Looks like {name} could use a good workout at the gym, don't you
-        think?"
-      </Typography>
-    </div>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm container>
+            <Grid
+              item
+              xs
+              container
+              direction="column"
+              spacing={2}
+              style={{
+                fontWeight: "bold",
+                marginTop: "5rem",
+                marginLeft: "1rem",
+              }}
+            >
+              <Grid item xs key={character}>
+                <Typography
+                  variant="h3"
+                  style={{
+                    fontFamily: "'Rock Salt', cursive",
+                    marginRight: "10rem",
+                  }}
+                  fontSize={90}
+                >
+                  {name}
+                </Typography>
+
+                <Typography
+                  variant="h6"
+                  style={{ fontWeight: "bold", marginTop: "5rem" }}
+                  fontSize={25}
+                  color="red"
+                >
+                  Biography
+                </Typography>
+                <Typography>
+                  Full Name: {character.biography.fullName}
+                </Typography>
+                <Typography>
+                  Alter Ego: {character.biography.alterEgos}
+                </Typography>
+                <Typography>
+                  Aliase(s): {character.biography.aliases.join(", ")}
+                </Typography>
+                <Typography>
+                  Place Of Birth: {character.biography.placeOfBirth}
+                </Typography>
+                <Typography>
+                  First Appearance(s): {character.biography.firstAppearance}
+                </Typography>
+
+                <Typography>{`Publisher: ${biography.publisher}`}</Typography>
+                <Typography>{`Alignment: ${biography.alignment}`}</Typography>
+                <br />
+
+                <Typography
+                  variant="h6"
+                  style={{ fontWeight: "bold" }}
+                  fontSize={25}
+                  borderRadius={30}
+                  color="red"
+                >
+                  Appearance
+                </Typography>
+                <Typography>{`Gender: ${appearance.gender}`}</Typography>
+                <Typography>{`Species: ${appearance.race}`}</Typography>
+                <Typography>{`Eyes: ${appearance.eyeColor}`}</Typography>
+                <Typography>{`Hair: ${appearance.hairColor}`}</Typography>
+
+                <Typography>{`Height: ${appearance.height.join(
+                  ", "
+                )}, Weight: ${appearance.weight.join(", ")}`}</Typography>
+                <br />
+
+                <Typography
+                  variant="h6"
+                  style={{ fontWeight: "bold" }}
+                  fontSize={25}
+                  color="red"
+                >
+                  Work
+                </Typography>
+                <Typography>{`Occupation: ${work.occupation}`}</Typography>
+                <Typography>{`Base: ${work.base}`}</Typography>
+                <br />
+
+                <Typography
+                  variant="h6"
+                  style={{ fontWeight: "bold" }}
+                  fontSize={25}
+                  color="red"
+                >
+                  Connections
+                </Typography>
+                <Typography>{`Group Affiliation: ${connections.groupAffiliation}`}</Typography>
+                <Typography>{`Family: ${connections.relatives}`}</Typography>
+              </Grid>
+            </Grid>
+            <Grid item marginRight={5} width="25%">
+              <img
+                src={images.md}
+                alt={name}
+                style={{
+                  marginTop: "2rem",
+                  // marginRight: "10rem",
+                }}
+              />
+
+              <Typography
+                variant="h6"
+                style={{
+                  fontWeight: "bold",
+                  marginTop: "1rem",
+                  textAlign: "center",
+                  // marginRight: "10rem",
+                }}
+                fontSize={25}
+                color="red"
+              >
+                Powerstats
+              </Typography>
+
+              <Typography sx={{ width: "90%", marginTop: "1rem" }}>
+                <label sx={{ width: "200px", textAlign: "right" }}>
+                  Intelligence : {powerstats.intelligence}%
+                </label>
+                <LinearProgress
+                  sx={{ height: 15, borderRadius: 5 }}
+                  variant="determinate"
+                  value={powerstats.intelligence}
+                />
+                <br />
+                <label sx={{ width: "200px", textAlign: "right" }}>
+                  Strength : {powerstats.strength}%
+                </label>
+                <LinearProgress
+                  sx={{ height: 15, borderRadius: 5, color: "red" }}
+                  variant="determinate"
+                  value={powerstats.strength}
+                />
+                <br />
+                <label sx={{ width: "200px", textAlign: "right" }}>
+                  Speed : {powerstats.speed}%
+                </label>
+                <LinearProgress
+                  sx={{ height: 15, borderRadius: 5 }}
+                  variant="determinate"
+                  value={powerstats.speed}
+                />
+                <br />
+                <label sx={{ width: "200px", textAlign: "right" }}>
+                  Durability : {powerstats.durability}%
+                </label>
+                <LinearProgress
+                  sx={{ height: 15, borderRadius: 5 }}
+                  variant="determinate"
+                  value={powerstats.durability}
+                />
+                <br />
+                <label sx={{ width: "200px", textAlign: "right" }}>
+                  Power : {powerstats.power}%
+                </label>
+                <LinearProgress
+                  sx={{ height: 15, borderRadius: 5 }}
+                  variant="determinate"
+                  value={powerstats.power}
+                />
+                <br />
+                <label sx={{ width: "200px", textAlign: "right" }}>
+                  Combat : {powerstats.combat}%
+                </label>
+                <LinearProgress
+                  sx={{ height: 15, borderRadius: 5 }}
+                  variant="determinate"
+                  value={powerstats.combat}
+                />
+              </Typography>
+              <br />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Box>
   );
 }
--- src/components/Home/CharOfTheDay.js
+++ src/components/Home/CharOfTheDay.js
@@ -5,9 +5,11 @@ import {
   CardActionArea,
   CardContent,
   CardMedia,
+  Grid,
   Typography,
   LinearProgress,
 } from "@mui/material";
+import { useCharsContext } from "../../context/CharsContext";
 
 export default function CharOfTheDay() {
   const [hero, setHero] = useState(null);
@@ -26,93 +28,116 @@ export default function CharOfTheDay() {
 
   return (
     <Card
-      sx={{ marginTop: 10, marginRight: 20, marginLeft: 20, marginBottom: 20 }}
+      sx={{
+        marginTop: 10,
+        marginRight: 20,
+        marginLeft: 20,
+        marginBottom: 20,
+      }}
     >
-      <CardActionArea sx={{ display: "flex" }}>
-        {hero ? (
-          <>
-            <CardMedia
-              sx={{ flex: "1 1 50%" }}
-              component="img"
-              height="640"
-              image={hero.images.sm}
-              alt="character of the day"
-            />
+      {hero ? (
+        <Grid container className="main-COTD" direction="row">
+          <Grid item className="image-COTD" width="24%">
+            <div class="comic">
+              <div class="panel">
+                <img src={hero.images.md} alt={hero.name} />
+                <p class="text top-left">The hero has come...</p>
+                <p class="text bottom-right">...to save the day!</p>
+              </div>
+            </div>
+          </Grid>
+          <Grid container className="info-COTD" direction="column" width="76%">
+            <Grid item className="bio-COTD" direction="row" height="50%">
+              <div class="comic">
+                <div class="panel">
+                  <h1 xs={{ paddingLeft: 20 }}>{hero.biography.fullName} </h1>
 
-            <CardContent sx={{ flex: "1 1 50%", width: "100%" }}>
-              <Typography gutterBottom variant="h1" component="div">
-                {hero.name}
-              </Typography>
-
-              <Typography variant="h6">Powerstats</Typography>
-
-              <Typography sx={{ width: "50%" }}>
-                <label sx={{ width: "200px", textAlign: "right" }}>
-                  Intelligence : {hero.powerstats.intelligence}%
-                </label>
-                <LinearProgress
-                  sx={{ height: 15, borderRadius: 5 }}
-                  variant="determinate"
-                  value={hero.powerstats.intelligence}
-                />
-                <br />
-                <label sx={{ width: "200px", textAlign: "right" }}>
-                  Strength : {hero.powerstats.strength}%
-                </label>
-                <LinearProgress
-                  sx={{ height: 15, borderRadius: 5 }}
-                  variant="determinate"
-                  value={hero.powerstats.strength}
-                />
-                <br />
-                <label sx={{ width: "200px", textAlign: "right" }}>
-                  Speed : {hero.powerstats.speed}%
-                </label>
-                <LinearProgress
-                  sx={{ height: 15, borderRadius: 5 }}
-                  variant="determinate"
-                  value={hero.powerstats.speed}
-                />
-                <br />
-                <label sx={{ width: "200px", textAlign: "right" }}>
-                  Durability : {hero.powerstats.durability}%
-                </label>
-                <LinearProgress
-                  sx={{ height: 15, borderRadius: 5 }}
-                  variant="determinate"
-                  value={hero.powerstats.durability}
-                />
-                <br />
-                <label sx={{ width: "200px", textAlign: "right" }}>
-                  Power : {hero.powerstats.power}%
-                </label>
-                <LinearProgress
-                  sx={{ height: 15, borderRadius: 5 }}
-                  variant="determinate"
-                  value={hero.powerstats.power}
-                />
-                <br />
-                <label sx={{ width: "200px", textAlign: "right" }}>
-                  Combat : {hero.powerstats.combat}%
-                </label>
-                <LinearProgress
-                  sx={{ height: 15, borderRadius: 5 }}
-                  variant="determinate"
-                  value={hero.powerstats.combat}
-                />
-              </Typography>
-            </CardContent>
-
-            <CardContent sx={{ flex: "1 1 50%", width: "100%" }}>
-              <Typography variant="body2" color="text.secondary">
-                {hero.biography.publisher}{" "}
-              </Typography>
-            </CardContent>
-          </>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </CardActionArea>
+                  <p class="speech">
+                    Get ready to meet the newest comic book character on the
+                    block - {hero.biography.fullName}, also known as{" "}
+                    {hero.biography.aliases.join(", ")}! Born in{" "}
+                    {hero.biography.placeOfBirth}, they made their first
+                    appearance in {hero.biography.firstAppearance}, published by{" "}
+                    {hero.biography.publisher}. Is they a hero or a villain?
+                    You'll just have to read and find out. One thing's for sure
+                    - they has connections all over the place and is a bit of a
+                    wildcard when it comes to group affiliations. And let's not
+                    forget their family - they play a big role in their life.
+                    Get ready for some wild adventures - this is going to be one
+                    epic ride!
+                  </p>
+                </div>{" "}
+              </div>
+            </Grid>
+            <Grid
+              container
+              className="stats-COTD"
+              direction="row"
+              height="50%"
+              zIndex={1}
+            >
+              <div class="comic">
+                <div class="panel">
+                  <Grid item className="left-stats" direction="column">
+                    <Typography>
+                      Intelligence : {hero.powerstats.intelligence}%
+                    </Typography>
+                    <LinearProgress
+                      sx={{ height: 15, borderRadius: 5 }}
+                      variant="determinate"
+                      value={hero.powerstats.intelligence}
+                    />
+                    <Typography>
+                      Strength : {hero.powerstats.strength}%
+                      <LinearProgress
+                        sx={{ height: 15, borderRadius: 5 }}
+                        variant="determinate"
+                        value={hero.powerstats.strength}
+                      />
+                    </Typography>
+                    <Typography>
+                      Speed : {hero.powerstats.speed}%
+                      <LinearProgress
+                        sx={{ height: 15, borderRadius: 5 }}
+                        variant="determinate"
+                        value={hero.powerstats.speed}
+                      />
+                    </Typography>
+                  </Grid>
+                  <Grid item className="right-stats" direction="column">
+                    <Typography>
+                      Durability : {hero.powerstats.durability}%
+                      <LinearProgress
+                        sx={{ height: 15, borderRadius: 5 }}
+                        variant="determinate"
+                        value={hero.powerstats.durability}
+                      />
+                    </Typography>
+                    <Typography>
+                      Power : {hero.powerstats.power}%
+                      <LinearProgress
+                        sx={{ height: 15, borderRadius: 5 }}
+                        variant="determinate"
+                        value={hero.powerstats.power}
+                      />
+                    </Typography>
+                    <Typography>
+                      Combat : {hero.powerstats.combat}%
+                      <LinearProgress
+                        sx={{ height: 15, borderRadius: 5 }}
+                        variant="determinate"
+                        value={hero.powerstats.combat}
+                      />
+                    </Typography>
+                  </Grid>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+        </Grid>
+      ) : (
+        <p>Loading...</p>
+      )}
     </Card>
   );
 }
