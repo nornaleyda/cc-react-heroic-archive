76a31357ba739fe92e64727b532c69928331281c
nzmksk <mhafiz9676@gmail.com>
2023-04-11 11:25:49 +0800

    FINAL PUSH :)

--- src/components/Home/CharOfTheDay.js
+++ src/components/Home/CharOfTheDay.js
@@ -1,15 +1,9 @@
 import { useState, useEffect } from "react";
 import "./layout.scss";
-import {
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Grid,
-  Typography,
-  LinearProgress,
-} from "@mui/material";
-import { useCharsContext } from "../../context/CharsContext";
+import { Button, Card, Grid, Typography, LinearProgress } from "@mui/material";
+import { Link } from "react-router-dom";
+import CharacterCard from "../Characters/DisplayResults/CharacterCard";
+import { red } from "@mui/material/colors";
 
 export default function CharOfTheDay() {
   const [hero, setHero] = useState(null);
@@ -37,20 +31,20 @@ export default function CharOfTheDay() {
     >
       {hero ? (
         <Grid container className="main-COTD" direction="row">
-          <Grid item className="image-COTD" width="24%">
+          <Grid item className="image-COTD" height="100%" width="23%">
             <div class="comic">
               <div class="panel">
                 <img src={hero.images.md} alt={hero.name} />
-                <p class="text top-left">The hero has come...</p>
-                <p class="text bottom-right">...to save the day!</p>
+                <p class="text1 top-left">Was it a hero...</p>
+                <p class="text2 bottom-right">...or a villain?</p>
               </div>
             </div>
           </Grid>
           <Grid container className="info-COTD" direction="column" width="76%">
             <Grid item className="bio-COTD" direction="row" height="50%">
               <div class="comic">
-                <div class="panel">
-                  <h1 xs={{ paddingLeft: 20 }}>{hero.biography.fullName} </h1>
+                <div class="panel1">
+                  <h1 className="name">{hero.biography.fullName} </h1>
 
                   <p class="speech">
                     Get ready to meet the newest comic book character on the
@@ -62,32 +56,37 @@ export default function CharOfTheDay() {
                     You'll just have to read and find out. One thing's for sure
                     - they has connections all over the place and is a bit of a
                     wildcard when it comes to group affiliations. And let's not
-                    forget their family - they play a big role in their life.
-                    Get ready for some wild adventures - this is going to be one
+                    forget their family - it plays a big role in their life. Get
+                    ready for some wild adventures - this is going to be one
                     epic ride!
                   </p>
-                </div>{" "}
+                </div>
               </div>
             </Grid>
-            <Grid
-              container
-              className="stats-COTD"
-              direction="row"
-              height="50%"
-              zIndex={1}
-            >
-              <div class="comic">
-                <div class="panel">
-                  <Grid item className="left-stats" direction="column">
-                    <Typography>
+            <div class="comic">
+              <div class="panel2">
+                <Grid
+                  container
+                  className="stats-COTD"
+                  direction="row"
+                  height="50%"
+                >
+                  <Grid
+                    item
+                    className="left-stats"
+                    direction="column"
+                    width="50%"
+                  >
+                    <Typography sx={{ width: "80%" }}>
                       Intelligence : {hero.powerstats.intelligence}%
+                      <LinearProgress
+                        sx={{ height: 15, borderRadius: 5 }}
+                        variant="determinate"
+                        value={hero.powerstats.intelligence}
+                      />{" "}
                     </Typography>
-                    <LinearProgress
-                      sx={{ height: 15, borderRadius: 5 }}
-                      variant="determinate"
-                      value={hero.powerstats.intelligence}
-                    />
-                    <Typography>
+
+                    <Typography sx={{ width: "80%" }}>
                       Strength : {hero.powerstats.strength}%
                       <LinearProgress
                         sx={{ height: 15, borderRadius: 5 }}
@@ -95,7 +94,7 @@ export default function CharOfTheDay() {
                         value={hero.powerstats.strength}
                       />
                     </Typography>
-                    <Typography>
+                    <Typography sx={{ width: "80%" }}>
                       Speed : {hero.powerstats.speed}%
                       <LinearProgress
                         sx={{ height: 15, borderRadius: 5 }}
@@ -104,8 +103,13 @@ export default function CharOfTheDay() {
                       />
                     </Typography>
                   </Grid>
-                  <Grid item className="right-stats" direction="column">
-                    <Typography>
+                  <Grid
+                    item
+                    className="right-stats"
+                    direction="column"
+                    width="50%"
+                  >
+                    <Typography sx={{ width: "80%" }}>
                       Durability : {hero.powerstats.durability}%
                       <LinearProgress
                         sx={{ height: 15, borderRadius: 5 }}
@@ -113,7 +117,7 @@ export default function CharOfTheDay() {
                         value={hero.powerstats.durability}
                       />
                     </Typography>
-                    <Typography>
+                    <Typography sx={{ width: "80%" }}>
                       Power : {hero.powerstats.power}%
                       <LinearProgress
                         sx={{ height: 15, borderRadius: 5 }}
@@ -121,7 +125,7 @@ export default function CharOfTheDay() {
                         value={hero.powerstats.power}
                       />
                     </Typography>
-                    <Typography>
+                    <Typography sx={{ width: "80%" }}>
                       Combat : {hero.powerstats.combat}%
                       <LinearProgress
                         sx={{ height: 15, borderRadius: 5 }}
@@ -130,14 +134,34 @@ export default function CharOfTheDay() {
                       />
                     </Typography>
                   </Grid>
-                </div>
+                </Grid>
               </div>
-            </Grid>
+            </div>
           </Grid>
         </Grid>
       ) : (
         <p>Loading...</p>
       )}
+      {hero && (
+        <Link to={`/character/${hero.id}`}>
+          <Button variant="contained"
+          sx={{
+            margin: 5,
+            backgroundColor: "red",
+            width: "35vw",
+          }}
+          >view</Button>
+        </Link>
+      )}
+      <Link to={`/characters`}>
+        <Button variant="contained"
+                  sx={{
+                    margin: 5,
+                    backgroundColor: "red",
+                    width: "38vw",
+                  }}
+        >all character</Button>
+      </Link>
     </Card>
   );
 }
