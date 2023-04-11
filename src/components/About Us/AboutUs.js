3f8ebcc3b1ba47e0a1ed1608aa881403585b4283
nzmksk <mhafiz9676@gmail.com>
2023-04-11 11:13:40 +0800

    Add profile image

--- src/components/About Us/AboutUs.js
+++ src/components/About Us/AboutUs.js
@@ -5,6 +5,7 @@ import "../../styles/main.scss";
 import johanan from "../../images/joeee.jpeg";
 import farhana from "../../images/farhana.jpeg";
 import norna from "../../images/norna.jpeg";
+import hafiz from "../../images/hafiz.jpg";
 
 const devDetails = [
   {
@@ -12,7 +13,7 @@ const devDetails = [
     name: "Mohd Hafiz Zabba",
     biography:
       "Watch out, villains - Hafiz is on the scene! Despite coming from a health science background, this self-taught coding genius has one thing in common with the Spider-Man - making websites. Starting his coding journey back in June 2022, he ventured out into the jungle of data analytics with Python. It wasn't long before Hafiz started swinging into web development, quickly mastering HTML, CSS, and JavaScript. Now, he's expanding his arsenal with React.js, Express.js, and Django. Whether you're a hero or a villain, Hafiz is the coder you want on your side!",
-    image: "",
+    image: hafiz,
     url: {
       github: "https://github.com/nzmksk",
       linkedIn: "https://www.linkedin.com/in/mhz96/",
